4a5c580cd222b40e591ec4af3184387f0228cbb1
HarithaNawarathna <nawarathnammhr.21@uom.lk>
2025-02-14 10:35:34 +0530

    google maps setup

--- src/components/Maps/MapExample.js
+++ src/components/Maps/MapExample.js
@@ -1,89 +1,80 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function MapExample() {
-  const mapRef = React.useRef(null);
-  React.useEffect(() => {
-    let google = window.google;
-    let map = mapRef.current;
-    let lat = "40.748817";
-    let lng = "-73.985428";
-    const myLatlng = new google.maps.LatLng(lat, lng);
-    const mapOptions = {
-      zoom: 12,
-      center: myLatlng,
-      scrollwheel: false,
-      zoomControl: true,
-      styles: [
-        {
-          featureType: "administrative",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#444444" }],
-        },
-        {
-          featureType: "landscape",
-          elementType: "all",
-          stylers: [{ color: "#f2f2f2" }],
-        },
-        {
-          featureType: "poi",
-          elementType: "all",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "road",
-          elementType: "all",
-          stylers: [{ saturation: -100 }, { lightness: 45 }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "all",
-          stylers: [{ visibility: "simplified" }],
-        },
-        {
-          featureType: "road.arterial",
-          elementType: "labels.icon",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "transit",
-          elementType: "all",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "water",
-          elementType: "all",
-          stylers: [{ color: "#4299e1" }, { visibility: "on" }],
-        },
-      ],
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const loadGoogleMaps = () => {
+      if (!window.google) {
+        console.error("google maps script not loaded");
+        return;
+      }
+
+      const lat = 40.748817;
+      const lng = -73.985428;
+      const myLatlng = new window.google.maps.LatLng(lat, lng);
+
+      const mapOptions = {
+        zoom: 12,
+        center: myLatlng,
+        scrollwheel: false,
+        zoomControl: true,
+        styles: [
+          {
+            featureType: "administrative",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#444444" }],
+          },
+          {
+            featureType: "landscape",
+            elementType: "all",
+            stylers: [{ color: "#f2f2f2" }],
+          },
+          {
+            featureType: "water",
+            elementType: "all",
+            stylers: [{ color: "#4299e1" }, { visibility: "on" }],
+          },
+        ],
+      };
+
+      const map = new window.google.maps.Map(mapRef.current, mapOptions);
+
+      const marker = new window.google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        animation: window.google.maps.Animation.DROP,
+        title: "notus react!",
+      });
+
+      const infowindow = new window.google.maps.InfoWindow({
+        content: `<div class="info-window-content">
+            <h2>Notus React</h2>
+            <p>A free admin for tailwind css, react, and react hooks.</p>
+          </div>`,
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open(map, marker);
+      });
     };
 
-    map = new google.maps.Map(map, mapOptions);
+    if (!window.google) {
+      const script = document.createElement("script");
+      script.src = `https://maps.googleapis.com/maps/api/js?key=your_google_maps_api_key`;
+      script.async = true;
+      script.defer = true;
+      script.onload = loadGoogleMaps;
+      document.body.appendChild(script);
+    } else {
+      loadGoogleMaps();
+    }
+  }, []);
 
-    const marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: "Notus React!",
-    });
-
-    const contentString =
-      '<div class="info-window-content"><h2>Notus React</h2>' +
-      "<p>A free Admin for Tailwind CSS, React, and React Hooks.</p></div>";
-
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
-
-    google.maps.event.addListener(marker, "click", function () {
-      infowindow.open(map, marker);
-    });
-  });
   return (
-    <>
-      <div className="relative w-full rounded h-600-px">
-        <div className="rounded h-full" ref={mapRef} />
-      </div>
-    </>
+    <div className="relative w-full rounded h-600-px">
+      <div className="rounded h-full" ref={mapRef} style={{ height: "600px" }} />
+    </div>
   );
 }
 
